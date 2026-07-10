c5ac2930f04a297b283b294153f3b42f23be22fd
Games Workin <gamesworkin@gmail.com>
2026-07-10 00:14:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-
-console.log("Portal em modo de construção ativa...");
-
-// Aumenta a velocidade de rotação da animação ao interagir
-document.addEventListener('mousemove', (e) => {
-    const loader = document.querySelector('.status-loader');
-    loader.style.animationDuration = '0.5s';
-});
+console.log("Portal carregado com sucesso.");
